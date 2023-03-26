36bdf6977d988b5a169814b455c8cffe2e1ec2cc
imnyang <nyang.nyanglab@gmail.com>
2023-03-26 21:11:48 +0900

    test

--- discordbot.py
+++ discordbot.py
@@ -1,25 +1,37 @@
+from cmath import log
+from distutils.sysconfig import PREFIX
 import discord
 import PingPongWr
+from dotenv import load_dotenv
 import os
+load_dotenv()
 
-from discord.ext import commands
+PREFIX = os.environ['PREFIX']
+TOKEN = os.environ['TOKEN']
 
-bot = commands.Bot(command_prefix='시루야 ',intents=discord.Intents.all())
+client = discord.Client()
 
 url = os.environ['PINGPONG_URL']  # 핑퐁빌더 Custom API URL
 pingpong_token = os.environ['PINGPONG_TOKEN']  # 핑퐁빌더 Custom API Token
 
 Ping = PingPongWr.Connect(url, pingpong_token)  # 핑퐁 모듈 클래스 선언
 
-@bot.event()
-async def on_message(message):
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}.')
 
-    if message.author == bot.user:
-            return
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
     if message.content.startswith("시루야"):
         str_text = (message.content.split(" "))[1].replace('시루야 ', '')
         return_data = await Ping.Pong(session_id ="Example", text = str_text, topic = True, image = True, dialog = True) # 핑퐁빌더 API에 Post 요청
         await message.channel.reply(return_data["text"])
 
-bot.run(os.environ['DISCORD_TOKEN'])
+
+try:
+    client.run(os.environ['DISCORD_TOKEN'])
+except discord.errors.LoginFailure as e:
+    print("Improper token has been passed.")
